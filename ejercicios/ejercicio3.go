0010b6c87708bbf56dbd2839c36017312004a069
MerMes16 <imerinomesa@gmail.com>
2025-05-19 20:51:01 -0300

    E3

--- ejercicios/ejercicio3.go
+++ ejercicios/ejercicio3.go
@@ -1,11 +1,32 @@
 package ejercicios
 
+import "sort"
+
 type Objeto struct {
 	Nombre string
 	Peso   int
 	Valor  int
 }
 
+func (o *Objeto) ValorPorUnidadDePeso() float32 {
+	return float32(o.Valor) / float32(o.Peso)
+}
+
 func Ejercicio3(objetos []Objeto, capacidad int) map[Objeto]float32 {
-	panic("No implementado")
+	sort.Slice(objetos, func(i, j int) bool {
+		return objetos[i].ValorPorUnidadDePeso() > objetos[j].ValorPorUnidadDePeso()
+	})
+
+	res, cap := make(map[Objeto]float32), 0
+	for _, obj := range objetos {
+		if cap+obj.Peso <= capacidad {
+			res[obj] = 1
+			cap += obj.Peso
+		} else {
+			pesoRestante := capacidad - cap
+			res[obj] = float32(pesoRestante) / float32(obj.Peso)
+			break
+		}
+	}
+	return res
 }
