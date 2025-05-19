d5ba480a4059d792787c40cab21452148cfce715
MerMes16 <imerinomesa@gmail.com>
2025-05-19 20:22:09 -0300

    E5

--- ejercicios/ejercicio5.go
+++ ejercicios/ejercicio5.go
@@ -3,5 +3,20 @@ package ejercicios
 var billetes = []int{10000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1}
 
 func Cambiar(cantidad int) map[int]int {
-	return nil
+	cambio := make(map[int]int)
+	for _, denominacion := range billetes {
+		if cantidad >= denominacion {
+
+			cantidadBilletes := cantidad / denominacion
+			cambio[denominacion] = cantidadBilletes
+			cantidad %= denominacion
+
+			if cantidad > 0 {
+				for k, v := range Cambiar(cantidad) {
+					cambio[k] += v
+				}
+			}
+		}
+	}
+	return cambio
 }
