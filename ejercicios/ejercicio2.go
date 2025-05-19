b5a82ad9538e289ab3fe3176fcbaaa539036fd18
MerMes16 <imerinomesa@gmail.com>
2025-05-19 20:03:58 -0300

    E2

--- ejercicios/ejercicio2.go
+++ ejercicios/ejercicio2.go
@@ -6,5 +6,14 @@ type Tarea struct {
 }
 
 func Ejercicio2(tareas []Tarea) {
-	panic("No implementado")
+	if len(tareas) < 2 {
+		return
+	}
+	for i := 1; i < len(tareas)-1; i++ {
+		for j := 0; j < len(tareas)-i; j++ {
+			if tareas[j].Tiempo > tareas[j+1].Tiempo {
+				tareas[j], tareas[j+1] = tareas[j+1], tareas[j]
+			}
+		}
+	}
 }
