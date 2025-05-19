cdcc6c6b27b6ebda4faa94bbad5e7439ccb13de4
MerMes16 <imerinomesa@gmail.com>
2025-05-19 19:49:34 -0300

    E1

--- ejercicios/ejercicio1.go
+++ ejercicios/ejercicio1.go
@@ -7,5 +7,15 @@ type Actividad struct {
 }
 
 func SelectorRecursivo(actividades []Actividad) []Actividad {
-	panic("No implementado")
+	seleccionadas := []Actividad{}
+	if len(actividades) == 0 {
+		return seleccionadas
+	}
+	actual, seleccionadas := actividades[0], append(seleccionadas, actividades[0])
+	for i, actividad := range actividades {
+		if actividad.Inicio > actual.Fin {
+			return append(seleccionadas, SelectorRecursivo(actividades[i:])...)
+		}
+	}
+	return seleccionadas
 }
